f052872c39ae0486a0cfe389968ceae7ae454fe8
David Um <dave5623@yahoo.com>
2012-08-04 01:46:22 -0400

    Committing code up to 3.1 (pg. 33)

--- my_debugger.py
+++ my_debugger.py
@@ -7,7 +7,9 @@ kernel32 = windll.kernel32
 
 class debugger():
     def __init__(self):
-        pass
+        self.h_process = None
+        self.pid = None
+        self.debugger_active = None
 
     def load(self, path_to_exe):
         creation_flags = DEBUG_PROCESS
@@ -34,3 +36,43 @@ class debugger():
             print("[*] PID: %d" % process_information.dwProcessId)
         else:
             print("[*] Error: 0x%08x." % kernel32.GetLastError())
+
+    def open_process(self, pid):
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, pid, False)
+        return h_process
+
+    def attach(self, pid):
+        self.h_process = self.open_process(pid)
+
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid = int(pid)
+            self.run()
+        else:
+            print "[*] Unable to attach to the process"
+            print "[*] Error: %d" % kernel32.GetLastError()
+
+    def run(self):
+        while self.debugger_active == True:
+            self.get_debug_event()
+
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+
+        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+            raw_input("Press button to continue")
+            self.debugger_active = False
+            kernel32.ContinueDebugEvent(\
+                debug_event.dwProcessId,\
+                debug_event.dwThreadId,\
+                continue_status)
+
+    def detach(self):
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print "[*] Finished debugging. Exiting..."
+            return True
+        else:
+            print "There was an error!"
+            return False
+
--- my_test.py
+++ my_test.py
@@ -4,4 +4,10 @@ import my_debugger
 
 debugger = my_debugger.debugger()
 
-debugger.load("C:\\Windows\\system32\\calc.exe")
+#debugger.load("C:\\Windows\\system32\\calc.exe")
+
+pid = raw_input("Enter the PID of the process to attach to:")
+
+debugger.attach(int(pid))
+
+debugger.detach()
